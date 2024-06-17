83eb97d1ef8de1aa95ec812ae1a8685a94072e94
gt.park <smileboy0014@gmail.com>
2024-06-18 00:40:24 +0900

    test: "특정 유저의 포인트를 충전하는 기능에 대한 테스트 작성"
    - PointControllerTest 작성
    - PointServiceTest 작성
    - UserPointTableTest 작성

--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,12 +1,15 @@
 package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.point.domain.UserPoint;
+import io.hhplus.tdd.point.exception.ErrorCode;
+import io.hhplus.tdd.point.exception.PointException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class PointServiceTest {
@@ -18,12 +21,41 @@ class PointServiceTest {
     @Test
     void getPoint() {
         //given
-        long userId = 1L;
+        long userId = 1;
 
         // when
         UserPoint result = pointService.getPoint(userId);
 
         //then
         assertThat(result.point()).isEqualTo(0);
+    }
+
+    @DisplayName("받은 포인트만큼 포인트를 충전한다.")
+    @Test
+    void charge(){
+        //given
+        long userId = 1;
+        long amount = 1000;
+
+        //when
+        UserPoint result = pointService.charge(userId, amount);
+
+        //then
+        assertThat(result.point()).isEqualTo(amount);
+    }
+
+    @DisplayName("0 미만의 포인트를 충전하려고하면 예외를 반환한다.")
+    @Test
+    void chargeInvalidPoint(){
+        //given
+        long userId = 1;
+        long amount = -1000;
+
+        //when //then
+        assertThatThrownBy(() -> pointService.charge(userId, amount))
+                .isInstanceOf(PointException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.INVALID_CHARGE_POINT);
+
     }
 }
