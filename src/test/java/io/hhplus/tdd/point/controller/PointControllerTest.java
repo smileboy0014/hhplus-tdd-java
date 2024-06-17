8698f8fb4eedcc66c721404cc5d6a73b32d69727
gt.park <smileboy0014@gmail.com>
2024-06-18 00:40:24 +0900

    test: "특정 유저의 포인트를 충전하는 기능에 대한 테스트 작성"
    - PointControllerTest 작성
    - PointServiceTest 작성
    - UserPointTableTest 작성

--- src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.hhplus.tdd.point.domain.UserPoint;
 import io.hhplus.tdd.point.service.PointService;
 import org.junit.jupiter.api.DisplayName;
@@ -7,11 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,6 +27,9 @@ class PointControllerTest {
     @MockBean
     private PointService pointService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @DisplayName("유저의 포인트를 조회한다")
     @Test
     void point() throws Exception {
@@ -33,12 +38,31 @@ class PointControllerTest {
         UserPoint result = new UserPoint(userId, 1000, System.currentTimeMillis());
         when(pointService.getPoint(userId)).thenReturn(result);
 
-
         //when //then
         mockMvc.perform(get("/point/%s".formatted(userId)))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.point").value(1000));
+    }
+
+    @DisplayName("특정 유저의 포인트를 충전한다.")
+    @Test
+    void charge() throws Exception {
+        //given
+        long userId = 1;
+        long amount = 1000;
+        UserPoint result = new UserPoint(userId, amount, System.currentTimeMillis());
+        when(pointService.charge(userId, amount)).thenReturn(result);
+
+        //when //then
+        mockMvc.perform(patch("/point/%s/charge".formatted(userId))
+                        .content(objectMapper.writeValueAsString(amount))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.point").value(amount));
+
     }
 }
