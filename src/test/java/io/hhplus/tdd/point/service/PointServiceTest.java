a9066485d88ea037a79e8a5112ad0fa58b3d0b0f
gt.park <smileboy0014@gmail.com>
2024-06-18 01:46:07 +0900

    test: "특정 유저의 포인트를 사용하는 기능에 대한 테스트 작성"
    - PointControllerTest 작성
    - PointServiceTest 작성

--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,13 +1,15 @@
 package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.point.domain.UserPoint;
-import io.hhplus.tdd.point.exception.ErrorCode;
 import io.hhplus.tdd.point.exception.PointException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static io.hhplus.tdd.point.exception.ErrorCode.INVALID_CHARGE_POINT;
+import static io.hhplus.tdd.point.exception.ErrorCode.NOT_ENOUGH_POINT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,6 +18,14 @@ class PointServiceTest {
 
     @Autowired
     private PointService pointService;
+
+    @Autowired
+    private CommonService commonService;
+
+    @AfterEach
+    void tearDown() {
+        commonService.cleanUserPointTable(1);
+    }
 
     @DisplayName("유저 id를 받아, 해당 유저의 포인트를 조회한다")
     @Test
@@ -55,7 +65,51 @@ class PointServiceTest {
         assertThatThrownBy(() -> pointService.charge(userId, amount))
                 .isInstanceOf(PointException.class)
                 .extracting("errorCode")
-                .isEqualTo(ErrorCode.INVALID_CHARGE_POINT);
+                .isEqualTo(INVALID_CHARGE_POINT);
+
+    }
+
+    @DisplayName("사용하는 포인트만큼 차감이 된다.")
+    @Test
+    void use(){
+        //given
+        pointService.charge(1, 10000);
+
+        //when
+        UserPoint result = pointService.use(1, 1000);
+
+        //then
+        assertThat(result.point()).isEqualTo(9000);
+    }
+
+    @DisplayName("0 미만의 포인트를 사용하려고 하면 예외를 반환한다.")
+    @Test
+    void useInvalidPoint(){
+        //given
+        long userId = 1;
+        long amount = -1000;
+
+        //when //then
+        assertThatThrownBy(() -> pointService.use(userId, amount))
+                .isInstanceOf(PointException.class)
+                .extracting("errorCode")
+                .isEqualTo(INVALID_CHARGE_POINT);
+
+    }
+
+    @DisplayName("가지고 있는 포인트 이상의 포인트를 사용하려고 하면 예외를 반환한다.")
+    @Test
+    void useOverPoint(){
+        //given
+        long userId = 1;
+        long amount = 15000;
+        pointService.charge(1, 10000);
+
+        //when //then
+        assertThatThrownBy(() -> pointService.use(userId, amount))
+                .isInstanceOf(PointException.class)
+                .extracting("errorCode")
+                .isEqualTo(NOT_ENOUGH_POINT);
 
     }
 }
